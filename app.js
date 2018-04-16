d95387f8e224b2bf4c439748ed7a29e1d4f9535d
Michelle  McGinty <michelle.mcginty@mycit.ie>
2018-04-16 10:41:03 +0100

    One more test

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 // Express Session middleware
 app.use(session({
